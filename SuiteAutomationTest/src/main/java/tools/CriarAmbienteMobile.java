8d4ef6675d837bafcd41f3b716dd36a7657fd21b
Juliana <juliana@chaordicsystems.com>
2017-07-06 00:18:06 -0300

    comment unused code

--- SuiteAutomationTest/src/main/java/tools/CriarAmbienteMobile.java
+++ SuiteAutomationTest/src/main/java/tools/CriarAmbienteMobile.java
@@ -1,37 +1,37 @@
-/**
- * @author Eduardo C. de Sá
- *
- * 13/08/2015
- */
-package tools;
+// /**
+//  * @author Eduardo C. de Sá
+//  *
+//  * 13/08/2015
+//  */
+// package tools;
 
-import java.net.MalformedURLException;
+// import java.net.MalformedURLException;
 
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
+// import org.testng.annotations.BeforeTest;
+// import org.testng.annotations.Optional;
+// import org.testng.annotations.Parameters;
 
-/**
- * @author eduardo.sa
- *
- */
-public class CriarAmbienteMobile {
+// /**
+//  * @author eduardo.sa
+//  *
+//  */
+// public class CriarAmbienteMobile {
 
-	@Parameters({ "so", "navegador", "versao" })
-	@BeforeTest
-	public void iniciarInstancia(String so, @Optional String navegador,@Optional String versao) throws MalformedURLException {
-		System.out.println("----> Início dos testes Mobile: "+ System.currentTimeMillis());
-		System.out.println("----> Instânciando o Driver");
-		System.out.println("----> Plataforma: " + so);
-		System.out.println("----> Navegador: " + navegador);
-		System.out.println("----> Versão: " + versao);
-		WebdriverMobile.criarInstanciaMobile();
-	}
+// 	@Parameters({ "so", "navegador", "versao" })
+// 	@BeforeTest
+// 	public void iniciarInstancia(String so, @Optional String navegador,@Optional String versao) throws MalformedURLException {
+// 		System.out.println("----> Início dos testes Mobile: "+ System.currentTimeMillis());
+// 		System.out.println("----> Instânciando o Driver");
+// 		System.out.println("----> Plataforma: " + so);
+// 		System.out.println("----> Navegador: " + navegador);
+// 		System.out.println("----> Versão: " + versao);
+// 		WebdriverMobile.criarInstanciaMobile();
+// 	}
 
-	//	@AfterTest
-	public void finalizar(@Optional String version) {
-		System.out.println("----> Finalizando o Driver: " + version);
-		WebdriverWeb.fecharInstancia();
-		System.out.println("----> Fim dos testes: " + System.currentTimeMillis());
-	}
-}
+// 	//	@AfterTest
+// 	public void finalizar(@Optional String version) {
+// 		System.out.println("----> Finalizando o Driver: " + version);
+// 		WebdriverWeb.fecharInstancia();
+// 		System.out.println("----> Fim dos testes: " + System.currentTimeMillis());
+// 	}
+// }
--- SuiteAutomationTest/src/main/java/tools/WebdriverMobile.java
+++ SuiteAutomationTest/src/main/java/tools/WebdriverMobile.java
@@ -1,58 +1,58 @@
-/**
- * @author Eduardo C. de Sá
- *
- * 13/08/2015
- */
-package tools;
+// /**
+//  * @author Eduardo C. de Sá
+//  *
+//  * 13/08/2015
+//  */
+// package tools;
 
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.MobileBrowserType;
-import io.appium.java_client.remote.MobileCapabilityType;
-import io.appium.java_client.remote.MobilePlatform;
+// import io.appium.java_client.android.AndroidDriver;
+// import io.appium.java_client.remote.MobileBrowserType;
+// import io.appium.java_client.remote.MobileCapabilityType;
+// import io.appium.java_client.remote.MobilePlatform;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+// import java.net.MalformedURLException;
+// import java.net.URL;
 
-import org.openqa.selenium.remote.DesiredCapabilities;
+// import org.openqa.selenium.remote.DesiredCapabilities;
 
-/**
- * @author eduardo.sa
- *
- */
-public class WebdriverMobile {
+// /**
+//  * @author eduardo.sa
+//  *
+//  */
+// public class WebdriverMobile {
 
-	@SuppressWarnings("rawtypes")
-	private static AndroidDriver driver;
+// 	@SuppressWarnings("rawtypes")
+// 	private static AndroidDriver driver;
 
-	@SuppressWarnings("rawtypes")
-	public static void criarInstanciaMobile() throws MalformedURLException {
+// 	@SuppressWarnings("rawtypes")
+// 	public static void criarInstanciaMobile() throws MalformedURLException {
 
-		DesiredCapabilities capacidade = new DesiredCapabilities();
-		capacidade.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
-		capacidade.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
-		//	capacidade.setCapability("browserName", MobileBrowserType.BROWSER);
-		capacidade.setCapability("browserName", MobileBrowserType.CHROME);
+// 		DesiredCapabilities capacidade = new DesiredCapabilities();
+// 		capacidade.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+// 		capacidade.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+// 		//	capacidade.setCapability("browserName", MobileBrowserType.BROWSER);
+// 		capacidade.setCapability("browserName", MobileBrowserType.CHROME);
 
-		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capacidade);
+// 		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capacidade);
 
-	}
-	@SuppressWarnings("rawtypes")
-	public static AndroidDriver obterInstancia() {
-		if (driver == null) {
-			System.out.println("-->> Driver == null");
-		}
-		return driver;
-	}
+// 	}
+// 	@SuppressWarnings("rawtypes")
+// 	public static AndroidDriver obterInstancia() {
+// 		if (driver == null) {
+// 			System.out.println("-->> Driver == null");
+// 		}
+// 		return driver;
+// 	}
 
-	public static void fecharInstancia() {
-		if (driver != null) {
-			driver.quit();
-			driver = null;
-		}
-	}
+// 	public static void fecharInstancia() {
+// 		if (driver != null) {
+// 			driver.quit();
+// 			driver = null;
+// 		}
+// 	}
 
-	public static void carregarUrl(String url) {
-		driver.get(url);
-	}
+// 	public static void carregarUrl(String url) {
+// 		driver.get(url);
+// 	}
 
-}
+// }
