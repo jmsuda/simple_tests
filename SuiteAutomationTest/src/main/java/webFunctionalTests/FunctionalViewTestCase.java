3dcb361b38f75ef629d5de39bda0d2d65f8a55ef
Juliana <juliana@chaordicsystems.com>
2017-07-06 16:46:37 -0300

    Fix class references

---
+++ SuiteAutomationTest/src/main/java/webFunctionalTests/FunctionalViewTestCase.java
@@ -0,0 +1,48 @@
+/**
+ * @author Eduardo C. de Sa
+ *
+ * 20/04/2016
+ */
+package webFunctionalTests;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+import pages.Platform;
+
+/**
+ * @author eduardo.sa
+ *
+ */
+public class FunctionalViewTestCase {
+
+	@Test
+	public void testViewProducts() throws IOException, ParseException {
+
+		Platform platform = new Platform();
+		ArrayList<String> urlProductPlatform = new ArrayList<String>();
+		ArrayList<String> anonymousUserIdList = new ArrayList<String>();
+		ArrayList<String> listIdProductAccess = new ArrayList<String>();
+		ArrayList<String> listIdProductPlatform = new ArrayList<String>(); 
+
+		String apiKey = "cec";
+		int quantProd = 5;
+
+		System.out.println("----> Testes de view de produtos no cliente: "+apiKey);
+
+		listIdProductPlatform = platform.getIdProductsPlatform(apiKey);
+		urlProductPlatform = platform.getUrlProductsByRankigProducts(apiKey, quantProd, listIdProductPlatform);
+		anonymousUserIdList = platform.acessarPaginasProduto(urlProductPlatform);
+		Assert.assertTrue(platform.verificarAnonymousUserId(anonymousUserIdList));
+		listIdProductAccess = platform.verificarViews(apiKey, anonymousUserIdList);		
+		Assert.assertTrue(platform.verificarIdProducts(listIdProductAccess, listIdProductPlatform));
+
+	}
+}
+
+
+
